4cbd4d87f8a8a97c28d393bb69387a2c8b2398d0
Mohnish <pmohnish@outlook.com>
2024-06-17 03:41:14 -0400

    Updated

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,26 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    let expenseTotal=0;
+    for(let i=0;i<expenses.length;i++){
+        expenseTotal += expenses[i].cost;
+    }
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        if(event.target.value>20000){
+            alert("The value should not exceed 20000");
+        } else if(event.target.value<expenseTotal){
+            alert("You cannot reduce the budget lower than spending");
+        }
+        else{
+            setNewBudget(event.target.value);
+        }
+
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
---
+++ src/components/Currency.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const Currency = () => {
+    const {currency,dispatch} = useContext(AppContext);
+    const handleCurrencyChange = (event) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.taget.value
+        });
+    }
+    return(
+        <div className='btn btn-primary' style={{display:'flex',alignItems:'center', backgroundColor:'green'}}>
+            
+            <span style={{marginRight:'10px'}}>Currency:</span>
+            <select value={currency}  onChange={handleCurrencyChange}>
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Rupee</option>
+            </select>
+        </div>
+
+    );
+}
+
+export default Currency;
